20ac6644ef653a91dc6962ba0d2c378d1eab1e9a
Dieres <matoshurtadodiegoaquiles@gmail.com>
2023-06-11 19:25:52 -0500

    Change date format to UTC to YYYY-MM-DD

--- helpers/calendar/addEvent.js
+++ helpers/calendar/addEvent.js
@@ -1,12 +1,14 @@
 import {db} from './../../firebase'
 import { addDoc, collection} from 'firebase/firestore'
 import {getItemAsync} from 'expo-secure-store'
+import dayjs from 'dayjs'
 export const addEvent =  async(title, description, date, setModalVisible, modalVisible) => {
     try{
+        const formatedDay = dayjs(date).format('YYYY-MM-DD')
         const auth = await getItemAsync('auth')
         const {userEmail} = JSON.parse(auth)
         await addDoc(collection(db, 'events'), {
-            title, description, date, owner : userEmail
+            title, description, date : formatedDay, owner : userEmail
         })
         setModalVisible(!modalVisible)
 
