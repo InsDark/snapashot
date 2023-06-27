a47a6ee3f297fc6efb37fb0997d4ddd0995f4b7a
Dieres <matoshurtadodiegoaquiles@gmail.com>
2023-06-27 13:10:48 -0500

    another eas config

--- app/index.js
+++ app/index.js
@@ -1,33 +1,34 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useEffect, useState } from 'react'
-import {getItemAsync, deleteItemAsync} from 'expo-secure-store'
-import SplashScreen from 'expo-splash-screen'
+import { getItemAsync, deleteItemAsync } from 'expo-secure-store'
+import { preventAutoHideAsync, hideAsync } from 'expo-splash-screen'
 import Auth from './auth'
 import Home from './home'
 
 const Index = () => {
-  SplashScreen.preventAutoHideAsync()
-  setTimeout(SplashScreen.hideAsync, 2000)
+  preventAutoHideAsync()
+  setTimeout(hideAsync, 2000)
   const [MainComponent, setMainComponent] = useState()
   useEffect(() => {
     const getAuth = async () => {
       const auth = await getItemAsync('auth')
-      if(auth) {
-        const dateNow = Date.now()
-        if(dateNow > auth.expiration) {
-            await deleteItemAsync('auth')
-            setMainComponent(<Auth/>)
-            return
-        }
-        setMainComponent(<Home/>)
+      if (!auth) {
+        setMainComponent(<Auth />)
         return
       }
-      setMainComponent(<Auth/>)
+      const dateNow = Date.now()
+      if (dateNow > auth.expiration) {
+        await deleteItemAsync('auth')
+        setMainComponent(<Auth />)
+        return
+      }
+      setMainComponent(<Home />)
+      return
     }
     getAuth()
-  },[])
+  }, [])
   return (
-    <SafeAreaView style={{ height: '100%'}}>
+    <SafeAreaView style={{ height: '100%' }}>
       {MainComponent}
     </SafeAreaView>
   )
